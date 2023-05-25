8345f07037ad48c83b99e4ceb668d12b3f2c0164
Jonathan Henrique Jeremias Souza <118951940+JonathanHJSouza@users.noreply.github.com>
2023-05-25 13:58:22 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,59 @@
+var numAleatorio = Math.floor(Math.random() * 100) + 1;
+var pontos = 100;
+var array = [];
+var stringArray = "";
+var maiorOuMenor = "";
+
+function novoJogo(){
+    
+    if (numDigitado == numAleatorio) {
+        numAleatorio = Math.floor(Math.random() * 100) + 1;
+        pontos = 100;
+        //console.log(numAleatorio);
+        document.getElementById("txtNumero").value = "";
+        var labelResposta = document.getElementById("labelResposta");
+        labelResposta.innerHTML = "Vamos jogar..."
+        array = [];
+        stringArray = "";
+        var labelTentativas = document.getElementById("labelTentativas");
+        labelTentativas.innerHTML = "...";
+    } else {
+        alert("Ainda não acertou, continue tentando!")
+    } 
+}
+
+function tentativa(){
+    numDigitado = parseInt(document.getElementById("txtNumero").value);
+
+    if(numDigitado != numAleatorio) {
+        //alert("errou")
+        pontos = pontos - 1;
+        var labelResposta = document.getElementById("labelResposta");
+        if (numDigitado < numAleatorio) {
+            maiorOuMenor = "Maior";
+        } else {
+            maiorOuMenor = "Menor";
+        }
+        labelResposta.innerHTML = "Errou! tente novamente. Você realizou " + (100 - pontos) + " tentativas." + " Tente um número " + maiorOuMenor
+
+    }else{
+        //alert("Parabéns, Você Acertou! fez "+ pontos + " pontos.")
+        var labelResposta = document.getElementById("labelResposta");
+        labelResposta.innerHTML = "Parabéns, Você Acertou! fez "+ pontos + " pontos."
+    }
+    console.log(numAleatorio)
+    
+    adicionarElementos();
+    stringArray = array.join(',');
+
+    var labelTentativas = document.getElementById("labelTentativas");
+    labelTentativas.innerHTML = stringArray;
+
+    document.getElementById("txtNumero").value = "";
+}
+
+function adicionarElementos() {
+    var input = document.getElementById("txtNumero");
+    var valores = input.value.split(",");
+    array.push(...valores);
+}
